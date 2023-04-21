7618fbc7e02c1c78e296ae4b57a8d6e306f2f443
tomasAybar <aybar.t4@gmail.com>
2023-04-21 11:01:19 -0300

    feat: navigation

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,52 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import { styles } from '../theme/appTheme'
+
+interface MenuItem {
+	name: string
+	icon: string
+	component: string
+}
+
+const menuItems = [
+	{
+		name: 'animation 101',
+		icon: 'cube-outline',
+		component: 'Animation101Screen',
+	},
+]
 
 export const HomeScreen = () => {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  );
-};
+	const renderMenuItem = (menuItem: MenuItem) => {
+		return (
+			<View>
+				<Text>
+					{menuItem.name} - {menuItem.icon}
+				</Text>
+			</View>
+		)
+	}
 
-const styles = StyleSheet.create({});
+	const renderListHeader = () => {
+		return (
+			<View>
+				<Text style={styles.title}>Opciones de Menu</Text>
+			</View>
+		)
+	}
+
+	return (
+		<View style={{ flex: 1 }}>
+			<Text style={styles.title}>Opciones de Menu</Text>
+
+			<FlatList
+				data={menuItems}
+				renderItem={({ item }) => renderMenuItem(item)}
+				keyExtractor={(item) => item.name}
+			/>
+		</View>
+	)
+}
+
+// const styles = StyleSheet.create({})
