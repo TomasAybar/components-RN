c1f1ef92c14696f982b02519a093adff99611cb1
tomasAybar <aybar.t4@gmail.com>
2023-05-08 22:38:42 -0300

    alert prompt que solo funciona en ios

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -24,11 +24,25 @@ export const AlertScreen = () => {
         );
     }
 
+    // Esto no funciona en android, solo funciona en IOS
+    // https://reactnative.dev/docs/alert#prompt-ios
+    const showPrompt = () => {
+        Alert.prompt(
+            'Esta seguro?', // titulo
+            'Esta accion no se puede revertir', // cuerpo de la alerta
+            (valor: string) => console.log('info: ', valor),
+            'plain-text', // es como el tipo de prompt, ver los demas valores, este es como el default
+            'Hola mundo', // valor por defecto del prompt
+            'number-pad' // el valor del teclado keyboardType
+        )
+    }
+
     return (
         <View style={styles.globalMargin}>
             <HeaderTitle title='Alertas' />
 
             <Button title='Mostrar alerta' onPress={showAlert} />
+            <Button title='Mostrar prompt' onPress={showPrompt} />
         </View>
     )
 }
